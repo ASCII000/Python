f1f67a1cf889de6e44d7eab6b53cc36f260ede8c
r4n_emerson <r4n.oficial@gmail.com>
2023-07-19 21:17:46 -0300

    Update _pands.py

--- pacotes/_pands.py
+++ pacotes/_pands.py
@@ -1,3 +1,14 @@
-import pandas
+import random, pandas
 
+frame = pandas.DataFrame(    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    })
 
+print(type(frame))
+print(frame)
